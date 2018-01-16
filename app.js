978289dfbed2a257c35cdfc5781d2c47e524dd25
DharaNaik <dhararnaik@gmail.com>
2018-01-16 13:05:29 -0500

    Added nunjucks

--- app.js
+++ app.js
@@ -5,35 +5,28 @@ const chalk = require('chalk');
 const nunjucks = require('nunjucks');
 //var router = express.Router();
 http.createServer(app).listen(3000);
-var locals = {
-  title : "A cool Twitter App",
-  people:[{name : "Danielle"},
+var person = [{name : "Danielle"},
           {name : "Dhara"},
           {name : "Emile"}
-        ]
-};
+        ];
 
-nunjucks.configure('views', {
-    //autoescape: true,
-    //express: app
-    noCache : true
-});
-
-nunjucks.render('index.html',locals,function(err, output){
-  console.log(output)
-});
+app.set('view engine', 'html')
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache : true});
+nunjucks.render('index.html',{title : "A cool Twitter App", person : person}
+);
 
 app.use('/', function (req, res, next){
     console.log(chalk.magenta(req.method), chalk.yellow(req.originalUrl), chalk.blue(res.statusCode));
     next();
 });
-// app.use(function (req, res){
-//     // if error throw error;
-//     res.sendStatus(200);
-// });
+
 app.get('/',function (req, res){
     res.sendStatus(200);
-
+    res.nunjucks.render('index.html',{title : "A cool Twitter App", person : person}
+    );
+  //  res.render('index', function(err, html) {
+  res.send(html);
 });
 
 //app.use('/', router);
