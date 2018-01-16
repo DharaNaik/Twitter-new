aa087c8de16b57e19b2d41f1bb46110ec9b4a6a7
DharaNaik <dhararnaik@gmail.com>
2018-01-16 12:37:30 -0500

    Added nunjucks

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require( 'express' );
 const app = express();
 var http = require("http");
 const chalk = require('chalk');
+const nunjucks = require('nunjucks');
 //var router = express.Router();
 http.createServer(app).listen(3000);
+var locals = {
+  title : "A cool Twitter App",
+  people:[{name : "Danielle"},
+          {name : "Dhara"},
+          {name : "Emile"}
+        ]
+};
+
+nunjucks.configure('views', {
+    //autoescape: true,
+    //express: app
+    noCache : true
+});
+
+nunjucks.render('index.html',locals,function(err, output){
+  console.log(output)
+});
+
 app.use('/', function (req, res, next){
     console.log(chalk.magenta(req.method), chalk.yellow(req.originalUrl), chalk.blue(res.statusCode));
     next();
@@ -14,12 +33,11 @@ app.use('/', function (req, res, next){
 // });
 app.get('/',function (req, res){
     res.sendStatus(200);
-    //res.render("testing");
-   // res.send("this work?");
+
 });
+
 //app.use('/', router);
 app.listen = function() {
     var server = http.createServer(this);
     return server.listen.apply(server, arg);
 }
-
